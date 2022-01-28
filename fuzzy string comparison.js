c488e8a936c993144b075e938f9e7cf212114b7b
Doctor Death D. Dracula <72820911+69Type@users.noreply.github.com>
2022-01-28 10:02:43 -0600

    Update fuzzy string comparison.js

--- fuzzy string comparison.js
+++ fuzzy string comparison.js
@@ -25,3 +25,19 @@ function stringSimilarity(str1, str2, gramSize = 2) {
   }
   return hits / total;
 }
+
+
+
+// another version
+
+function Compare(strA,strB){
+    for(var result = 0, i = strA.length; i--;){
+        if(typeof strB[i] == 'undefined' || strA[i] == strB[i]);
+        else if(strA[i].toLowerCase() == strB[i].toLowerCase())
+            result++;
+        else
+            result += 4;
+    }
+    return 1 - (result + 4*Math.abs(strA.length - strB.length))/(2*(strA.length+strB.length));
+}
+
