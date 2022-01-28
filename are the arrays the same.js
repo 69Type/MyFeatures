6d894b87e04f6a43f432e1a9e37931b93031399d
Doctor Death D. Dracula <72820911+69Type@users.noreply.github.com>
2022-01-28 09:06:26 -0600

    Update and rename isArraysTheSame.js to are the arrays the same.js

--- are the arrays the same.js
+++ are the arrays the same.js
@@ -1,3 +1,3 @@
-function isArraysTheSame(array1, array2){
+function areArraysTheSame(array1, array2){
     return array1.length !== array2.length ? false : !array1.some((el, i) => el !== array2[i]);
 }
