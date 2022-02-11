dd58e3c24055e818043dd97def26d34519dd0f8c
Doctor Death D. Dracula <72820911+69Type@users.noreply.github.com>
2022-02-11 11:32:52 -0600

    Create scriptFinder.js

---
+++ scriptFinder.js
@@ -0,0 +1,8 @@
+function(args){
+    Array.from(f('script', true)).forEach(async function(s){
+        let text = s.src ? await me.request(s.src) : s.textContent;
+        if (text.includesAll(args)){
+            console.log(s);
+        }
+    });
+}
