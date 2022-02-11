50d61f67c3dc59527cafe0fbb9c0194b879efa78
Doctor Death D. Dracula <72820911+69Type@users.noreply.github.com>
2022-02-11 11:33:05 -0600

    Update scriptFinder.js

--- scriptFinder.js
+++ scriptFinder.js
@@ -1,4 +1,4 @@
-function(args){
+function scriptFinder(args){
     Array.from(f('script', true)).forEach(async function(s){
         let text = s.src ? await me.request(s.src) : s.textContent;
         if (text.includesAll(args)){
