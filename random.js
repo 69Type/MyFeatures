057b55a40c38c8a620445ce3ab6754dc29fb9fcf
Doctor Death D. Dracula <72820911+69Type@users.noreply.github.com>
2022-02-13 13:08:57 -0600

    Update and rename range.js to random.js

--- random.js
+++ random.js
@@ -1,3 +1,7 @@
 function range(a, b, step=1){
     return Array(b - a).fill().map((_, i) => i + a).filter(k => k % step === 0);
 }
+
+function randNumStr(length){
+    return Array(length).fill().map(_ => String(Math.random())[2]).join('');
+}
