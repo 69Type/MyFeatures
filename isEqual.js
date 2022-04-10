c4e46d3a228cce85f858657388636910a1f1c9e7
Doctor Death D. Dracula <72820911+69Type@users.noreply.github.com>
2022-04-10 06:18:01 -0500

    Create isEqual.js

---
+++ isEqual.js
@@ -0,0 +1,28 @@
+/* Функция сложных объектов */
+function isEqual(a, b) {
+    /* перебор ключей в объекте */
+	for (key in a) {
+        /* сравнение типов */
+		if (typeof a[key] == typeof b[key]) {
+            /* провекра существования ключа в объекте B */
+			if (!(key in b)) {
+				return false;
+			}
+            /* провекра поля на объект */
+			if (typeof a[key] == "object") {
+                /* сравнение под-объектов */
+				if (!isEqual(a[key], b[key])) {
+					return false;
+				}
+			} else {
+                /* сравнение примитивных данных полей */
+				if (a[key] != b[key]) {
+					return false;
+				}
+			}
+		} else {
+			return false;
+		}
+	}
+	return true;
+}
